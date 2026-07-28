f0655a83dcf26c9b4e2d34917328380ee148a6f2
Dante Alves <dantealves8@gmail.com>
2026-07-28 15:06:23 -0300

    feat: adiciona interceptor para pegar accessToken do localStorage

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -5,4 +5,11 @@ const api = axios.create({
     headers: { 'Content-Type': 'application/json' },
 });
 
+api.intercerptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+})
 export default api;
